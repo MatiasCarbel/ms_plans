04eebdfb070b2d7b053a28b68787755c806fed29
MatiasCarbel <matiasjcarbel@gmail.com>
2022-11-10 18:58:52 -0300

    model

--- coupons/models.py
+++ coupons/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Coupon(models.Model):
+    code = models.CharField(max_length = 30) 
+    discount_percentage = models.PositiveIntegerField()
+    currency = models.CharField(max_length = 3)
+    
