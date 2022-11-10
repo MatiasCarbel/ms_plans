c8572159ad075041c7a16bb33c8f16b922bcaf34
MatiasCarbel <matiasjcarbel@gmail.com>
2022-11-10 19:05:12 -0300

    views and serializers

--- coupons/views.py
+++ coupons/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import viewsets
+from coupons.models import Coupon
+from coupons.serializers import CouponSerializer
 
 # Create your views here.
+
+class ListCoupons(viewsets.ViewSet):
+    
+    def list(self, request):
+        coupon_code = request.query_params.get('code')
+        queryset = Coupon.objects.all()
+
+        if coupon_code:
+            queryset = Coupon.objects.filter(code = coupon_code)
+
+        
+        serializer = CouponSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
